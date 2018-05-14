7a99ffe7026623541eca46436f3ed2130a8d9a26
Patrick Pang <mail@patrickpang.net>
2018-05-14 10:34:53 +1000

    Added paired t-tests.

--- jvlc2018.py
+++ jvlc2018.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from scipy.stats import ttest_rel
 
 from anova import do_anova
 from func import get_data_file_path
@@ -26,11 +27,29 @@ for g in groups:
     results = results.append(do_anova(f"Ease {g}", data_ease[data_ease.group == g], "vis_type", "value"))
 print(results.to_string())
 
+# Run t-tests on ease-of-use of all groups.
+print()
+print("UT/NT", ttest_rel(data_ease["value"][data_ease.vis_type == "UT"],
+                         data_ease["value"][data_ease.vis_type == "NT"]))
+print("UT/ML", ttest_rel(data_ease["value"][data_ease.vis_type == "UT"],
+                         data_ease["value"][data_ease.vis_type == "ML"]))
+print("NT/ML", ttest_rel(data_ease["value"][data_ease.vis_type == "NT"],
+                         data_ease["value"][data_ease.vis_type == "ML"]))
+
 print()
 results = do_anova("Help All", data_helpful, "vis_type", "value")
 for g in groups:
     results = results.append(do_anova(f"Help {g}", data_helpful[data_helpful.group == g], "vis_type", "value"))
 print(results.to_string())
+
+# Run t-tests on helpfulness of all groups.
+print()
+print("UT/NT", ttest_rel(data_helpful["value"][data_helpful.vis_type == "UT"],
+                         data_helpful["value"][data_helpful.vis_type == "NT"]))
+print("UT/ML", ttest_rel(data_helpful["value"][data_helpful.vis_type == "UT"],
+                         data_helpful["value"][data_helpful.vis_type == "ML"]))
+print("NT/ML", ttest_rel(data_helpful["value"][data_helpful.vis_type == "NT"],
+                         data_helpful["value"][data_helpful.vis_type == "ML"]))
 
 # Run ANOVA for speed.
 print()
